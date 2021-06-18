cd99c98635aa8ec8d086e2e3f6f869e298b27006
cweave_ <cweav3r@gmail.com>
2021-06-17 21:43:30 -0400

    update scraping

--- src/static/astro-data.js
+++ src/static/astro-data.js
@@ -1,38 +1,66 @@
-const rp = require('request-promise');
-const $ = require('cheerio');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 const fs = require('fs');
 
-const url = 'https://horoscopes.astro-seek.com/retrograde-planets-astrology-calendar-2020';
+const today = new Date();
+const year = today.getFullYear();
+
+const url = `https://horoscopes.astro-seek.com/retrograde-planets-astrology-calendar-${year}`;
 const planetSelector = '#vypocty_id_rocni .right-sedy-odsazeni a.tenky-modry';
 
-rp(url)
-	.then((html) => {
-		const retrogrades = $(planetSelector, html)
-			.map(function (i, el) {
-				const name = $(this).text();
-				const dateFields = $(this).parent().next().next().text();
-				// Formate date to match Mmm dd, yyyy
-				const regex = /[A-Za-z]{3} [0-9]{1,2}, [0-9]{4}/g;
-				const dates = [...dateFields.matchAll(regex)].map((match) => {
-					return match[0];
-				});
+const fetchData = async () => {
+	try {
+		// get html text from reddit
+		const response = await fetch(url);
+		// using await to ensure that the promise resolves
+		const body = await response.text();
 
-				return {
-					name,
-					dates
-				};
-			})
-			.get();
+		return body;
+	} catch (err) {
+		console.log('All errors are welcomed here! From promises or not, this catch is your catch.');
+	}
+};
 
-		// convert JSON object to string
-		const data = JSON.stringify(retrogrades, null, 4);
+const scrapeRetrogrades = async (body) => {
+	// parse the html text and extract titles
+	const $ = cheerio.load(body);
+	let retrogrades = [];
+	$(planetSelector).each((i, el) => {
+		const name = $(el).text();
+		const dateFields = $(el).parent().next().next().text();
+		// Formate date to match Mmm dd, yyyy
+		const regex = /[A-Za-z]{3} [0-9]{1,2}, [0-9]{4}/g;
+		const dates = [...dateFields.matchAll(regex)].map((match) => {
+			return match[0];
+		});
 
-		// write JSON string to a file
-		try {
-			fs.writeFileSync('retrograde-planets.json', data);
-			console.log('JSON data is saved.');
-		} catch (error) {
-			console.error(err);
-		}
-	})
-	.catch((err) => console.error(err));
+		retrogrades.push({
+			name,
+			dates
+		});
+	});
+
+	return retrogrades;
+};
+
+const writeToFile = async (retrogrades) => {
+	// convert JSON object to string
+	const data = JSON.stringify(retrogrades, null, 4);
+
+	// write JSON string to a file
+	try {
+		fs.writeFileSync('retrograde-planets.json', data);
+		console.log('JSON data is saved.');
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const fetchAndCreateRetrogradeData = async () => {
+	const astroseekData = await fetchData();
+	const htmlScrapedData = await scrapeRetrogrades(astroseekData);
+	const createRetrogradeJsonData = await writeToFile(htmlScrapedData);
+	return astroseekData, htmlScrapedData, createRetrogradeJsonData;
+};
+
+fetchAndCreateRetrogradeData().catch((error) => console.error(`${error}, there was an error creating the data.`));
