6ea4940859822554916200c92af28b86059c18c5
cweave <cweav3r@gmail.com>
2021-06-24 22:21:55 -0400

    card flips

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import Retrogrades from './Retrogrades';
 
 const App = () => {
 	return (
-		<div className="content-container">
-			<h1>What is currently in retrograde that is ruining my life?</h1>
-			<p>
-				Having a really bad week where nothing seems to be going right? Seem to be in a funk that doesn't make sense?
-				Here's the planet(s) to blame!
-			</p>
-			<Retrogrades />
-		</div>
+		<>
+			<div className="content-container">
+				<h1>What is currently in retrograde that is ruining my life?</h1>
+				<p>
+					Having a really bad week where nothing seems to be going right? Seem to be in a funk that doesn't make sense?
+					Here's the planet(s) to blame!
+				</p>
+			</div>
+			<div className="content-container">
+				<Retrogrades />
+			</div>
+		</>
 	);
 };
 
--- src/Planet.js
+++ src/Planet.js
@@ -3,6 +3,7 @@ import Icon from './svgs/index';
 
 const Planet = ({ name, dates }) => {
 	const [planetIsActive, setPlanetIsActive] = useState(false);
+	const [flipped, setFlipped] = useState(false);
 	const currentDate = new Date().toISOString();
 
 	const dateIsActive = (date) => {
@@ -17,18 +18,31 @@ const Planet = ({ name, dates }) => {
 		});
 	}, [name, dates, dateIsActive]);
 
-	return planetIsActive ? (
-		<div className="planet">
-			<Icon name={name} width={300} height={300} title={`${name} is currently in retrograde`} />
+	const handleFlipped = () => {
+		setFlipped(!flipped)
+	};
 
-			<h2 className="planet-name">{name}</h2>
-			{dates.map((date) =>
-				dateIsActive(date) ? (
-					<p key={`${name}-${new Date(date.start).toISOString()}`} className="retrograde-dates">
-						{date.start} &ndash; {date.end}
-					</p>
-				) : null
-			)}
+	return planetIsActive ? (
+		<div className={`planet ${flipped ? 'flipped' : null}`} onClick={() => !handleFlipped()}>
+			<div className="planet-card__inner">
+				<div className="planet-card__front">
+					<Icon name={name} width={300} height={300} title={`${name} is currently in retrograde`} />
+
+					<h2 className="planet-name">{name}</h2>
+					{dates.map((date) =>
+						dateIsActive(date) ? (
+							<p key={`${name}-${new Date(date.start).toISOString()}`} className="retrograde-dates">
+								{date.start} &ndash; {date.end}
+							</p>
+						) : null
+					)}
+				</div>
+				<div className="planet-card__back">
+					<h3 className="name">{name}</h3>
+					<p>What is effected?</p>
+					<p>We love that guy</p>
+				</div>
+			</div>
 		</div>
 	) : null;
 };
